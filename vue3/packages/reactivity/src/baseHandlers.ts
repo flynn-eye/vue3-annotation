789d287d68f094c0f8ca2477f24dee794a997657
flynn-eye <939649107@qq.com>
2021-02-02 21:31:10 +0800

    baseHandlers注释

--- vue3/packages/reactivity/src/baseHandlers.ts
+++ vue3/packages/reactivity/src/baseHandlers.ts
@@ -25,7 +25,13 @@ import {
   extend
 } from '@vue/shared'
 import { isRef } from './ref'
-
+/* 
+createSetter
+mutableHandlers
+readonlyHandlers
+shallowReactiveHandlers
+createGetter
+*/
 const builtInSymbols = new Set(
   Object.getOwnPropertyNames(Symbol)
     .map(key => (Symbol as any)[key])
@@ -42,6 +48,13 @@ const arrayInstrumentations: Record<string, Function> = {}
 // values
 ;(['includes', 'indexOf', 'lastIndexOf'] as const).forEach(key => {
   const method = Array.prototype[key] as any
+  /* 
+    相当于强化了上面的Array原型链上的三个方法，
+    每个方法都会track数组上的每一个值，之后再执行本身的操作
+    因为args可能已经被proxy了可能就会失败，
+    如果操作失败则toRaw拿到未被Proxy的值
+    return res -1 || false为失败
+  */
   arrayInstrumentations[key] = function(this: unknown[], ...args: unknown[]) {
     const arr = toRaw(this)
     for (let i = 0, l = this.length; i < l; i++) {
@@ -59,6 +72,10 @@ const arrayInstrumentations: Record<string, Function> = {}
 })
 // instrument length-altering mutation methods to avoid length being tracked
 // which leads to infinite loops in some cases (#2137)
+/* 
+这些方法都会更改长度，在某些情况死循环，所以暂停追踪track
+github可以看issue #2137
+*/
 ;(['push', 'pop', 'shift', 'unshift', 'splice'] as const).forEach(key => {
   const method = Array.prototype[key] as any
   arrayInstrumentations[key] = function(this: unknown[], ...args: unknown[]) {
@@ -71,6 +88,11 @@ const arrayInstrumentations: Record<string, Function> = {}
 
 function createGetter(isReadonly = false, shallow = false) {
   return function get(target: Target, key: string | symbol, receiver: object) {
+    /* 
+      key === __v_isReactive和 isReadonly取反
+      key === __v_isReadonly 返回isReadonly
+      key === __v_raw 返回 target
+    */
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly
     } else if (key === ReactiveFlags.IS_READONLY) {
@@ -81,7 +103,12 @@ function createGetter(isReadonly = false, shallow = false) {
     ) {
       return target
     }
-
+    /* 
+    判断target是数组
+    且调用'includes', 'indexOf', 'lastIndexOf'，
+    且有key
+    那么返回
+    */
     const targetIsArray = isArray(target)
 
     if (!isReadonly && targetIsArray && hasOwn(arrayInstrumentations, key)) {
@@ -89,7 +116,10 @@ function createGetter(isReadonly = false, shallow = false) {
     }
 
     const res = Reflect.get(target, key, receiver)
-
+    /* 
+    如果是系统内建的key symbol
+    那么直接返回值
+    */
     if (
       isSymbol(key)
         ? builtInSymbols.has(key as symbol)
@@ -97,7 +127,7 @@ function createGetter(isReadonly = false, shallow = false) {
     ) {
       return res
     }
-
+    // 只读不会进行追踪
     if (!isReadonly) {
       track(target, TrackOpTypes.GET, key)
     }
@@ -105,13 +135,15 @@ function createGetter(isReadonly = false, shallow = false) {
     if (shallow) {
       return res
     }
-
+    /* 
+    判断ref的返回res.value或res 
+    */
     if (isRef(res)) {
       // ref unwrapping - does not apply for Array + integer key.
       const shouldUnwrap = !targetIsArray || !isIntegerKey(key)
       return shouldUnwrap ? res.value : res
     }
-
+    //如果res对象更具isReadonly返回proxy对象
     if (isObject(res)) {
       // Convert returned value into a proxy as well. we do the isObject check
       // here to avoid invalid value warning. Also need to lazy access readonly
@@ -135,21 +167,34 @@ function createSetter(shallow = false) {
   ): boolean {
     const oldValue = (target as any)[key]
     if (!shallow) {
+      /* 
+        toRaw获取新value原始未proxy的值，
+        如果不是数组，且old值是ref新值不是ref则赋值
+      */
       value = toRaw(value)
       if (!isArray(target) && isRef(oldValue) && !isRef(value)) {
         oldValue.value = value
         return true
       }
     } else {
+      // shallow模式下都不变
       // in shallow mode, objects are set as-is regardless of reactive or not
     }
+    /* 
+    hadKey 
+    对象处理方法： hasOwn基于  Object.prototype.hasOwnProperty
+    判断自己有无这个属性继承过来的返回false
+    数组处理方法：判断数组下标是否合法
 
+    */
     const hadKey =
       isArray(target) && isIntegerKey(key)
         ? Number(key) < target.length
         : hasOwn(target, key)
     const result = Reflect.set(target, key, value, receiver)
     // don't trigger if target is something up in the prototype chain of original
+   // 如果target等于 receiver的未被proxy的值则触发trigger 
+   // 如果target是 receiver的raw的原型链上的值那么不触发trigger
     if (target === toRaw(receiver)) {
       if (!hadKey) {
         trigger(target, TriggerOpTypes.ADD, key, value)
@@ -183,15 +228,19 @@ function ownKeys(target: object): (string | number | symbol)[] {
   track(target, TrackOpTypes.ITERATE, isArray(target) ? 'length' : ITERATE_KEY)
   return Reflect.ownKeys(target)
 }
-
+/* 
+writeable的对象的handlers
+*/
 export const mutableHandlers: ProxyHandler<object> = {
-  get,
-  set,
-  deleteProperty,
-  has,
-  ownKeys
+  get,// 获取一个属性的value值的拦截操作
+  set,// 设置一个属性的value值的拦截操作
+  deleteProperty,// 删除一个属性的拦截操作
+  has,// 访问 in 操作符的拦截操作
+  ownKeys// 拦截访问key的值的操作
 }
-
+/* 
+readonly的对象的handlers
+*/
 export const readonlyHandlers: ProxyHandler<object> = {
   get: readonlyGet,
   set(target, key) {
@@ -213,7 +262,9 @@ export const readonlyHandlers: ProxyHandler<object> = {
     return true
   }
 }
-
+/* 
+extend 是Object.assign方法，为了合并对象
+*/
 export const shallowReactiveHandlers: ProxyHandler<object> = extend(
   {},
   mutableHandlers,
